f370c597679f3b75df8f74a441ac0be6bf7b87e4
Reece Kenney <reecekenney50@gmail.com>
2025-03-05 17:42:49 +0000

    Updates to schema for PC skill support

--- stanleySchema.js
+++ stanleySchema.js
@@ -19,8 +19,8 @@ export const StanleyResponse = z.object({
       "internet",
       "updates",
       "stocks",
-      "app",
-      "code",
+      "pc command",
+      "vs code command",
       "health",
       "blood pressure",
     ])
@@ -35,7 +35,72 @@ export const StanleyResponse = z.object({
       "trendRecently",
       "trendMonth",
       "trendYear",
+      "open",
+      "close",
     ])
     .optional(),
   room: z.string().optional(),
+
+  appNames: z
+    .array(
+      z.enum([
+        // Browsers
+        "microsoft edge",
+        "google chrome",
+        "firefox",
+        "opera",
+
+        // Microsoft Office Apps
+        "outlook",
+        "excel",
+        "word",
+        "powerpoint",
+        "onenote",
+
+        // Windows Apps
+        "calendar",
+        "people",
+        "mail",
+        "files",
+        "file explorer",
+        "notepad",
+        "calculator",
+        "settings",
+        "photos",
+        "store",
+
+        // Development Tools
+        "visual studio code",
+        "vs code",
+        "visual studio",
+        "github desktop",
+        "terminal",
+
+        // Communication Apps
+        "skype",
+        "teams",
+        "slack",
+        "discord",
+      ])
+    )
+    .optional(),
+
+  appCommands: z.array(z.enum(["open", "close", "minimize", "maximize"])),
+  vsCodeCommands: z.array(
+    z.enum([
+      "build",
+      "run",
+      "git log",
+      "close log",
+      "git pull",
+      "git status",
+      "git push",
+      "git commit",
+      "git checkout main",
+      "install dependencies",
+      "run tests",
+      "lint",
+      "storybook",
+    ])
+  ),
 });
